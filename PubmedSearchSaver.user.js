9ddc7a7ea7bdefeb4fc9fd6b7c2e69848395ba97
Durkud1357 <konrad.gruchal@gmail.com>
2025-12-28 23:30:16 +0100

    updated version

--- PubmedSearchSaver.user.js
+++ PubmedSearchSaver.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PubMedSearchSaver
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  Script that enhances PubMed browsing experience by highlighting articles from previous saved searches. Wesołych Świąt!
 // @author       Durkud1357
 // @match        https://*.han3.wum.edu.pl/*
